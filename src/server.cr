0a55e019574539d8c58680757a794364e6af07ec
Florius <j@florius.com.ar>
2017-06-13 00:25:14 -0300

    server con las neuvas reglas

--- src/server.cr
+++ src/server.cr
@@ -2,11 +2,14 @@ require "http/server"
 require "./vitix"
 
 config = Configuracion.new
-config.que cuandoEntroA: "/foo", vayaA: "http://google.com"
+
+regla = ReglaFactory.new.de("/foo").vallaA(["http://google.com", "http://google.com.ar"]).build
+config.agregarRegla regla
 
 tonel = Tonel.new config
 
 server = HTTP::Server.new(8080) do |context|
+  puts "atendiendo coneccion de #{context.request.path}"
   respuesta = tonel.transformar context.request.path
   context.response.print respuesta.contenido
 end
